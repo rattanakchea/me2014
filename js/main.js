713971b724fabf6d76e69156502bbdd0d35cb148
Rattanak Chea <rattanak.chea@gmail.com>
2015-05-15 01:37:48 -0700

    add featured project section, google font, clean js code

--- js/main.js
+++ js/main.js
@@ -6,32 +6,33 @@
       return _.template($('#'+id).html());  
     };
     
-    //cache DOM elements
-    app = {
-        template : templateFunc('projectTemplate'),
-        $data : $("#data")
-        
-    };
+    function builtTemplate(project, id, location){
+        var tpl = templateFunc(id);
+        var html = tpl(project);
+        //app.$data.append(html);
+        $('#'+location).append(html);
+    }
     
     $('.heartbeat').show();
-    $.getJSON('https://dl.dropboxusercontent.com/u/2122820/project.json', function(data){
+    $.getJSON('https://dl.dropboxusercontent.com/u/2122820/projects/project.json', function(data){
      //data = JSON.parse(data);
-       console.log(typeof(data));
         var projects = (data.projects);
-        console.log(projects);
-        
+        //console.log(projects);
         projects.forEach(function(project){
-           builtTemplate(project);
+           builtTemplate(project, 'projectTemplate', 'projectLocation');
             
         });
         $('.heartbeat').hide();
+        
+        
+        var featuredProjects = projects.slice(0, 3);
+        console.log(featuredProjects);
+        featuredProjects.forEach(function(f){
+            builtTemplate(f, 'featuredTemplate', 'featuredLocation');
+        });
+        
       
     });
-    
-    function builtTemplate(project){
-        var html = app.template(project);
-        app.$data.append(html);
-    }
     
     
 
